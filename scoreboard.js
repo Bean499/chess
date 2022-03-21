ec4e0f8e2770db86b2099cb1820da9b2b1d1767e
Ben Robinson <88161959+Bean499@users.noreply.github.com>
2022-03-21 15:29:51 +0000

    scoreboard update

--- scoreboard.js
+++ scoreboard.js
@@ -2,8 +2,6 @@ const electron = require("electron");
 const $ = require("jquery");
 const fs = require("fs");
 
-const BrowserWindow = electron.remote.BrowserWindow;
-
 function readScoreboard() {
     return fs.readFileSync("scoreboard.json", "utf8")
 }
@@ -12,17 +10,3 @@ function writeScoreboard(json) {
     json = JSON.stringify(json);
     fs.writeFileSync("scoreboard.json", json);
 }
-
-$("#scoreboard").click(() => {
-    win.webContents.executeJavaScript('const path = require("path");'
-    + 'const fs = require("fs");'
-    + 'fs.readFile(path.join(__dirname, "./scoreboard.json"), '
-    + '{encoding: "utf-8"}, function(err, data) {'
-    + 'if (!err) { console.log("received data: " + data); }'
-    + 'else { console.log(err); } });', true)
-    .then(console.log('JavaScript Executed Successfully'));
-});
-
-$("#write-scoreboard").click(() => {
-
-});
