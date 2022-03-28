fbd7893a28e78fe178fd2d60b4393836f59dc804
Bean499 <bentr99@gmail.com>
2022-03-28 14:39:15 +0100

    improved name validation

--- menus.js
+++ menus.js
@@ -45,8 +45,11 @@ $("#go").click(() => {
     localStorage.p2Name = p2Name;
     localStorage.timer = timer;
     //Check length of names
-    if (p1Name.length <= 0 || p2Name.length <= 0) {
-        $("#error").html("You need to enter a value for both names!");
+    if (p1Name.length < 2 || p2Name.length < 2 || p1Name.length > 12 || p2Name.length > 12) {
+        $("#error").html("You need to enter a value for both names that is between 2 and 12 characters (inclusive)");
+    }
+    else if (p1Name.match(/[|\\/~^:,;?!&%$@*+]/) || p2Name.match(/[|\\/~^:,;?!&%$@*+]/)) {
+        $("#error").html("Names cannot include symbols.");
     }
     //If names are input, go to the game
     else {
